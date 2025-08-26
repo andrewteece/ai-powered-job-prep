6672ee00363d8381e9fe11a576f126755856833d
andrewteece <andrewteece@gmail.com>
2025-08-26 15:52:51 -0500

    chore: update README

--- test/server.ts
+++
@@ -1,9 +0,0 @@
-import { setupServer } from 'msw/node';
-import { http, HttpResponse } from 'msw';
-
-// Add handlers as your API grows
-export const server = setupServer(
-  http.post('/api/interview/start', async () =>
-    HttpResponse.json({ id: 'mock-1' })
-  )
-);
